091a94dd471569d94fab9526eb8a86fec6086ccc
onish <cpanel.pc@gmail.com>
2020-05-29 15:44:51 +0530

    feat: Fetch Catalog by MerchantID

--- src/main/java/com/example/hyperlocal/HomeController.java
+++ src/main/java/com/example/hyperlocal/HomeController.java
@@ -2,8 +2,10 @@ package com.example.hyperlocal;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -22,10 +24,27 @@ public class HomeController {
 		return "Hello, world!";
 	}
 
+	@GetMapping("/api/v1/shops/{merchantID}")
+    public List getCatalog(@PathVariable("merchantID") String merchantID) {
+		return this.jdbcTemplate.queryForList("SELECT * FROM `Catalog` WHERE `MerchantID` = '"+merchantID+"'").stream()
+		.map((m) -> m.values().toString())
+		.collect(Collectors.toList());
+	}
+	
+	@GetMapping("/api/v2/shops/{merchantID}")
+    public CompletableFuture getCatalogAsync(@PathVariable("merchantID") String merchantID) {
+		CompletableFuture promise = CompletableFuture.supplyAsync(() -> {
+				return this.jdbcTemplate.queryForList("SELECT * FROM `Catalog` WHERE `MerchantID` = '"+merchantID+"'"); 
+			}).thenApply((itemsList) -> {
+				return itemsList.stream().map((m) -> m.values().toString()).collect(Collectors.toList());
+			});
+		return promise;
+	}
+
 	@GetMapping("/getMerchants")
 	public List<String> getMerchants() {
 		return this.jdbcTemplate.queryForList("SELECT * FROM `Merchants`").stream()
-				.map((m) -> m.values().toString())
-				.collect(Collectors.toList());
+		.map((m) -> m.values().toString())
+		.collect(Collectors.toList());
 	}
 }
