ce925cfa95ba586bd5c36f8aadba448a3bbc47c4
Diksha <dikshamodi@google.com>
2020-06-01 06:27:02 +0000

    testing with update
    Test: Jasync-sql with insert and update commands to CloudSQL database
    
    testing with update
    
    testing with update
    
    Test: Jasync-SQL with insert and delete commands to the CloudSQL
    database

--- src/main/java/com/example/hyperlocal/MerchantController.java
+++ src/main/java/com/example/hyperlocal/MerchantController.java
@@ -2,14 +2,16 @@ package com.example.hyperlocal;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.github.jasync.sql.db.Connection;
 import com.github.jasync.sql.db.QueryResult;
-import com.github.jasync.sql.db.ResultSet;
 import com.github.jasync.sql.db.mysql.MySQLConnectionBuilder;
+import com.google.api.client.json.JsonString;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,20 +29,42 @@ public class MerchantController {
         "jdbc:mysql:///hyperlocal?socketFactory=com.google.cloud.sql.mysql.SocketFactory&cloudSqlInstance=speedy-anthem-217710:us-central1:hyperlocal");
     CompletableFuture<List> future = CompletableFuture.supplyAsync(() -> {
       CompletableFuture<QueryResult> queryResult = connection
-          .sendPreparedStatement( 
-            String.format("Select * from Merchants where MerchantID = %s", id)
-          );
+          .sendPreparedStatement(String.format("Select * from Merchants where MerchantID = %s", id));
       return queryResult.join().getRows().get(0);
     });
     return future;
   }
 
-  @PostMapping("merchants") 
-  public void addCatalogItem() {
+  @PostMapping("/merchants") 
+  public void addCatalogItem(@RequestBody String postInputString) {
+    JsonObject jsonObject = JsonParser.parseString(postInputString).getAsJsonObject();
+    
+    Merchants newMerchant = new Merchants(
+    jsonObject.get("ShopName").getAsString(),
+     jsonObject.get("Latitude").getAsDouble(),
+     jsonObject.get("Longitude").getAsDouble(),
+     jsonObject.get("TypeOfService").getAsString(),
+     jsonObject.get("AddressLine1").getAsString()
+    );
+
     Connection connection = MySQLConnectionBuilder.createConnectionPool(
       "jdbc:mysql:///hyperlocal?socketFactory=com.google.cloud.sql.mysql.SocketFactory&cloudSqlInstance=speedy-anthem-217710:us-central1:hyperlocal");
       connection.sendPreparedStatement( 
-          String.format("INSERT INTO Merchants (`ShopName`, `Latitude`, `Longitude`, `TypeOfService`, `AddressLine1`) VALUES ('Dummy Shop', 13.63168521, 42.76488548, 'Groceries', 'Ring Colony');"
-      ));
-  }
+          String.format("INSERT INTO Merchants"
+          .concat("(`ShopName`, `Latitude`, `Longitude`, `TypeOfService`, `AddressLine1`)") 
+          .concat("VALUES (")
+          .concat(String.format("'%s',", newMerchant.getShopName()))
+          .concat(String.format("%s,", newMerchant.getLatitude()))
+          .concat(String.format("%s,", newMerchant.getLongitude()))
+          .concat(String.format("'%s',", newMerchant.getTypeOfService()))
+          .concat(String.format("'%s');", newMerchant.getAddressLine1())))
+      );    
+  }  
+
+  @PostMapping("merchants/update")
+  public void updateCatalogItem() {
+    Connection connection = MySQLConnectionBuilder.createConnectionPool(
+      "jdbc:mysql:///hyperlocal?socketFactory=com.google.cloud.sql.mysql.SocketFactory&cloudSqlInstance=speedy-anthem-217710:us-central1:hyperlocal");
+      connection.sendPreparedStatement("UPDATE Merchants SET ShopName =' a new name' where MerchantID=13");    
+  } 
 }
--- src/main/java/com/example/hyperlocal/Merchants.java
+++ src/main/java/com/example/hyperlocal/Merchants.java
@@ -1,15 +1,38 @@
 package com.example.hyperlocal;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class Merchants {
   private String ShopName; 
+  private double Latitude;
+  private double Longitude;
+  private String TypeOfService;
+  private String AddressLine;
 
-  public Merchants(String shop) {
-    this.ShopName = shop;
+
+  public Merchants(String shopName, Double latitude, Double longitude, String serviceType, String address) {
+    this.ShopName = shopName;
+    this.Latitude = latitude;
+    this.Longitude = longitude;
+    this.TypeOfService = serviceType;
+    this.AddressLine = address;
   }
+
   public String getShopName() {
     return ShopName;
+  }
+
+  public String getTypeOfService() {
+    return TypeOfService;
+  }
+
+  public String getAddressLine1() {
+    return AddressLine;
+  }
+
+  public Double getLatitude(){
+    return Latitude;
+  }
+
+  public Double getLongitude() {
+    return Longitude;
   }
 }
