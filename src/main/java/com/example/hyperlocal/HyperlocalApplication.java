0375a5f540c1f30428240e2255df5ae6239819b5
Diksha <dikshamodi@google.com>
2020-06-02 09:27:26 +0000

    Feat: Publish messages to pubSub topic
    Chain push to pubsub with new Merchant insertion
    
    Update addMerchant method to return CompleletableFuture<Merchant>: returns the new inserted merchant as CompletableFuture<Merchant>

--- src/main/java/com/example/hyperlocal/HyperlocalApplication.java
+++ src/main/java/com/example/hyperlocal/HyperlocalApplication.java
@@ -1,12 +1,15 @@
 package com.example.hyperlocal;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class HyperlocalApplication {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
 		SpringApplication.run(HyperlocalApplication.class, args);
 	}
 
--- src/main/java/com/example/hyperlocal/MerchantController.java
+++ src/main/java/com/example/hyperlocal/MerchantController.java
@@ -3,41 +3,49 @@ package com.example.hyperlocal;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.github.jasync.sql.db.Connection;
 import com.github.jasync.sql.db.QueryResult;
 import com.github.jasync.sql.db.mysql.MySQLConnectionBuilder;
-import com.google.api.client.json.JsonString;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-
+import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 // Rest Controller for Merchants
 @RestController
 public class MerchantController {
 
-  @GetMapping("/merchants/{id}")
-  public CompletableFuture<List> getCatalog(@PathVariable Integer id) {
+  private final PubSubTemplate publisher;
 
+  public MerchantController(PubSubTemplate pubSubTemplate) {
+    this.publisher = pubSubTemplate;
+  }
+
+  /* Route handlers for the Merchant API */
+
+  @GetMapping("/merchants/")
+  public CompletableFuture<List> getCatalog(/*@PathVariable Integer id*/) {
     Connection connection = MySQLConnectionBuilder.createConnectionPool(
         "jdbc:mysql:///hyperlocal?socketFactory=com.google.cloud.sql.mysql.SocketFactory&cloudSqlInstance=speedy-anthem-217710:us-central1:hyperlocal");
     CompletableFuture<List> future = CompletableFuture.supplyAsync(() -> {
       CompletableFuture<QueryResult> queryResult = connection
-          .sendPreparedStatement(String.format("Select * from Merchants where MerchantID = %s", id));
-      return queryResult.join().getRows().get(0);
+          .sendPreparedStatement("Select * from Merchants;");//String.format("Select * from Merchants where MerchantID = %s", id));
+      return queryResult.join().getRows();
     });
     return future;
   }
 
-  @PostMapping("/merchants") 
-  public void addCatalogItem(@RequestBody String postInputString) {
-    JsonObject jsonObject = JsonParser.parseString(postInputString).getAsJsonObject();
+
+  @PostMapping("/merchants/new") 
+  public CompletableFuture<Merchants> addMerchant(@RequestBody String postInputString) {
+    CompletableFuture<Merchants> newMerchantObject = CompletableFuture.supplyAsync(() -> {
+    JsonObject jsonObject = JsonParser.parseString(postInputString).getAsJsonObject(); 
     
     Merchants newMerchant = new Merchants(
     jsonObject.get("ShopName").getAsString(),
@@ -58,13 +66,28 @@ public class MerchantController {
           .concat(String.format("%s,", newMerchant.getLongitude()))
           .concat(String.format("'%s',", newMerchant.getTypeOfService()))
           .concat(String.format("'%s');", newMerchant.getAddressLine1())))
-      );    
+      );
+      return newMerchant;
+    });
+      
+    newMerchantObject = newMerchantObject.thenApply((newMerchant) -> {
+      publishMessage(postInputString);   
+      return newMerchant;
+    });
+    return newMerchantObject;
   }  
 
-  @PostMapping("merchants/update")
-  public void updateCatalogItem() {
+
+  @PostMapping("/merchants/update/{id}")
+  public void updateCatalogItem(@RequestBody String postInputString,
+       @RequestParam Integer MerchantID) {
     Connection connection = MySQLConnectionBuilder.createConnectionPool(
       "jdbc:mysql:///hyperlocal?socketFactory=com.google.cloud.sql.mysql.SocketFactory&cloudSqlInstance=speedy-anthem-217710:us-central1:hyperlocal");
       connection.sendPreparedStatement("UPDATE Merchants SET ShopName =' a new name' where MerchantID=13");    
   } 
+
+  public void publishMessage(String message) {
+    this.publisher.publish("projects/speedy-anthem-217710/topics/testTopic",message);
+  }
+
 }
