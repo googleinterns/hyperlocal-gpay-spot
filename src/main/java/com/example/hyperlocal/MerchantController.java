a0cb7bb72ac1ba6483fdfd616d0082a08a0c4906
Diksha <dikshamodi@google.com>
2020-06-04 06:56:33 +0000

    Merged Async operations with thenCompose

--- src/main/java/com/example/hyperlocal/MerchantController.java
+++ src/main/java/com/example/hyperlocal/MerchantController.java
@@ -3,9 +3,10 @@ package com.example.hyperlocal;
 import java.util.Collections;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import com.github.jasync.sql.db.Connection;
 import com.github.jasync.sql.db.QueryResult;
@@ -17,11 +18,11 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
 import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureAdapter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // Rest Controller for Merchants
@@ -37,95 +38,137 @@ public class MerchantController {
   @GetMapping("/get/shop/{shopID}")
   public CompletableFuture<String> getShopDetails(@PathVariable Integer shopID) {
     // DB Connection
-    Connection connection = MySQLConnectionBuilder.createConnectionPool("jdbc:mysql:///hyperlocal?socketFactory=com.google.cloud.sql.mysql.SocketFactory&cloudSqlInstance=speedy-anthem-217710:us-central1:hyperlocal");
-    
+    Connection connection = MySQLConnectionBuilder.createConnectionPool(
+        "jdbc:mysql:///hyperlocal?socketFactory=com.google.cloud.sql.mysql.SocketFactory&cloudSqlInstance=speedy-anthem-217710:us-central1:hyperlocal");
+
     // Promise: returns JSON string
     CompletableFuture<String> shopPromise = connection
-      .sendPreparedStatement("SELECT * from `Shops` WHERE `ShopID` = ?;", Arrays.asList(shopID))
-      .thenApply((QueryResult queryResult) -> {
-        // Get Shop details
-        ResultSet rows = queryResult.getRows();
-        if(rows.size() == 0) return null;
-        else
-        {
-          RowData shopData = rows.get(0);
-          return new Shop(shopData);
-        }
-      }).thenApply((Shop shop) -> {
-        // Shop exists?
-        if(shop == null) return new Gson().toJson(Collections.singletonMap("error", "Shop not found."));
-        else
-        {
-          // Get Merchant Details
-          CompletableFuture<Merchant> merchantPromise = connection
-            .sendPreparedStatement("SELECT * from `Merchants` WHERE `MerchantID` = ?;", Arrays.asList(shop.MerchantID))
-            .thenApply((QueryResult merchantQuery) -> {
-              RowData merchantData = merchantQuery.getRows().get(0);
-              return new Merchant((Long)merchantData.get(0), (String)merchantData.get(1), (String)merchantData.get(2));
-            });
-          
-          // Get Catalog Details
-          CompletableFuture<ArrayList<Service>> catalogPromise = connection
-            .sendPreparedStatement("SELECT * from `Catalog` WHERE `ShopID` = ?;", Arrays.asList(shop.ShopID))
-            .thenApply((QueryResult catalogQuery) -> {
-              ResultSet catalogSet = catalogQuery.getRows();
-              ArrayList<Service> serviceList = new ArrayList<Service>();
-              for(RowData serviceRow : catalogSet) serviceList.add(new Service(serviceRow));
-              return serviceList;
-            });
-          
-          // Put everything into HashMap
-          HashMap<String, Object> shopMap = new HashMap<String, Object>();
-          shopMap.put("ShopDetails", shop);
-          try
-          {
-            shopMap.put("MerchantDetails", merchantPromise.get());
-            shopMap.put("Catalog", catalogPromise.get());
+        .sendPreparedStatement("SELECT * from `Shops` WHERE `ShopID` = ?;", Arrays.asList(shopID))
+        .thenApply((QueryResult queryResult) -> {
+          // Get Shop details
+          ResultSet rows = queryResult.getRows();
+          if (rows.size() == 0)
+            return null;
+          else {
+            RowData shopData = rows.get(0);
+            return new Shop(shopData);
           }
-          catch(Exception e)
-          {
-            return new Gson().toJson(Collections.singletonMap("error", "Internal Server Error: Could not fetch details"));
-          }
+        }).thenApply((Shop shop) -> {
+          // Shop exists?
+          if (shop == null)
+            return new Gson().toJson(Collections.singletonMap("error", "Shop not found."));
+          else {
+            // Get Merchant Details
+            CompletableFuture<Merchant> merchantPromise = connection
+                .sendPreparedStatement("SELECT * from `Merchants` WHERE `MerchantID` = ?;",
+                    Arrays.asList(shop.MerchantID))
+                .thenApply((QueryResult merchantQuery) -> {
+                  RowData merchantData = merchantQuery.getRows().get(0);
+                  return new Merchant((Long) merchantData.get(0), (String) merchantData.get(1),
+                      (String) merchantData.get(2));
+                });
 
-          // Convert HashMap to JSON string
-          return new Gson().toJson(shopMap);
-        }
-      });
+            // Get Catalog Details
+            CompletableFuture<ArrayList<Service>> catalogPromise = connection
+                .sendPreparedStatement("SELECT * from `Catalog` WHERE `ShopID` = ?;", Arrays.asList(shop.ShopID))
+                .thenApply((QueryResult catalogQuery) -> {
+                  ResultSet catalogSet = catalogQuery.getRows();
+                  ArrayList<Service> serviceList = new ArrayList<Service>();
+                  for (RowData serviceRow : catalogSet)
+                    serviceList.add(new Service(serviceRow));
+                  return serviceList;
+                });
+
+            // Put everything into HashMap
+            HashMap<String, Object> shopMap = new HashMap<String, Object>();
+            shopMap.put("ShopDetails", shop);
+            try {
+              shopMap.put("MerchantDetails", merchantPromise.get());
+              shopMap.put("Catalog", catalogPromise.get());
+            } catch (Exception e) {
+              return new Gson()
+                  .toJson(Collections.singletonMap("error", "Internal Server Error: Could not fetch details"));
+            }
+
+            // Convert HashMap to JSON string
+            return new Gson().toJson(shopMap);
+          }
+        });
 
     return shopPromise;
   }
 
-  @PostMapping("/merchants/") 
-  public CompletableFuture<Merchants> addMerchant(@RequestBody String postInputString) {
-    JsonObject jsonObject = JsonParser.parseString(postInputString).getAsJsonObject(); 
-      
-    Merchants newMerchant = new Merchants(
-      jsonObject.get("MerchantID").getAsInt(),
-      jsonObject.get("MerchantName").getAsString(),
-      jsonObject.get("MerchantPhone").getAsString()
-    );
+  /* List of All merchants */
+
+  @GetMapping("/merchants/")
+  public CompletableFuture<List> getCatalog() {
+    Connection connection = MySQLConnectionBuilder.createConnectionPool(
+        "jdbc:mysql:///hyperlocal?socketFactory=com.google.cloud.sql.mysql.SocketFactory&cloudSqlInstance=speedy-anthem-217710:us-central1:hyperlocal");
+    CompletableFuture<List> future = CompletableFuture.supplyAsync(() -> {
+      CompletableFuture<QueryResult> queryResult = connection
+          .sendPreparedStatement("Select * from Merchants;");
+      return queryResult.join().getRows();
+    });
+    return future;
+  }
+
+
+  /*
+    Upserts details of a merchant
+      - MerchantID
+      - MerchantName
+      - MerchantPhone
+
+    Returns: Merchant Object with updated Details if exists, else return inserted Merchant object
+  */
+
+  @PostMapping("/merchants")
+  public CompletableFuture<Merchant> addMerchant(@RequestBody String postInputString) {
+    JsonObject jsonObject = JsonParser.parseString(postInputString).getAsJsonObject();
+
+    Merchant newMerchant = new Merchant(jsonObject);
 
     Connection connection = MySQLConnectionBuilder.createConnectionPool(
-      "jdbc:mysql:///hyperlocal?socketFactory=com.google.cloud.sql.mysql.SocketFactory&cloudSqlInstance=speedy-anthem-217710:us-central1:hyperlocal");   
+        "jdbc:mysql:///hyperlocal?socketFactory=com.google.cloud.sql.mysql.SocketFactory&cloudSqlInstance=speedy-anthem-217710:us-central1:hyperlocal");
 
-    CompletableFuture<Merchants> insertedMerchant = connection
-        .sendPreparedStatement(
-      String.format("INSERT INTO Merchants"
-      .concat("(`MerchantID`, `MerchantName`, `MerchantPhone`)") 
-      .concat("VALUES (")
-      .concat(String.format("'%s',", newMerchant.getMerchantID()))
-      .concat(String.format("'%s',", newMerchant.getMerchantName()))
-      .concat(String.format("'%s');", newMerchant.getMerchantPhone())))
-    ).thenApply((result) -> {
-      ListenableFuture<String> publishPromise = publishMessage(postInputString); // what to do with this?
-      return newMerchant;
-    });
-
-    return insertedMerchant;
+    CompletableFuture<Merchant> upsertedMerchantDetails = connection
+      .sendPreparedStatement("SELECT COUNT(1) from Merchants where MerchantID = ?;",
+          Arrays.asList(newMerchant.getMerchantID()))
+      .thenCompose(queryResult -> {
+        if (queryResult.getRows().size() != 0) {
+          return connection.sendPreparedStatement(
+            "UPDATE `Merchants` SET "
+            .concat(String.format("`MerchantName` = '%s',", newMerchant.getMerchantName()))
+            .concat(String.format("`MerchantPhone` = '%s' ", newMerchant.getMerchantPhone()))
+            .concat(String.format("WHERE `MerchantID`= %s", newMerchant.getMerchantID())));
+        }
+        else{
+          return connection.sendPreparedStatement(
+              String.format("INSERT INTO Merchants".concat("(`MerchantID`, `MerchantName`, `MerchantPhone`)")
+                  .concat("VALUES (").concat(String.format("'%s',", newMerchant.getMerchantID()))
+                  .concat(String.format("'%s',", newMerchant.getMerchantName()))
+                  .concat(String.format("'%s');", newMerchant.getMerchantPhone()))));
+        }
+      })
+      .thenApply((result) -> {
+        //Something is wrong here: Debugging it
+        // String publishPromiseAsString=null;
+        // try {
+        //   publishPromiseAsString = publishMessage(postInputString).get();
+        // } catch (InterruptedException | ExecutionException e) {
+        //   e.printStackTrace();
+        // }
+        Merchant insertedMerchant = new Merchant(
+         JsonParser.parseString(postInputString).getAsJsonObject()
+        );
+        return insertedMerchant;
+      }); 
+    return upsertedMerchantDetails;
   }  
 
-  public ListenableFuture<String> publishMessage(String message) {
-    return this.publisher.publish("projects/speedy-anthem-217710/topics/testTopic",message);
+
+  public CompletableFuture<String> publishMessage(String message) {
+    return this.publisher.publish("projects/speedy-anthem-217710/topics/testTopic",message).completable();
   }
 
 }
