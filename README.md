746c59b597dbe9c2cce12ba04f7d6ddec7319ba6
Diksha <dikshamodi@google.com>
2020-06-05 04:04:15 +0000

    Setup: Initial Setup and Build files

--- README.md
+++
@@ -1,1 +0,0 @@
-# hyperlocal-gpay-spot
--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.listen(8080, () => {"Listening at port 8080\n"});
