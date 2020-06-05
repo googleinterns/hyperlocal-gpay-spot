f87ea5d24da9f92f92795f57f4bbac1854cc7790
Diksha <dikshamodi@google.com>
2020-06-05 03:47:51 +0000

    Setup: Initial Setup and Build files

--- src/main/java/com/example/hyperlocal/HyperlocalApplication.java
+++ src/main/java/com/example/hyperlocal/HyperlocalApplication.java
@@ -4,12 +4,6 @@ import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
-import org.springframework.cloud.gcp.pubsub.integration.outbound.PubSubMessageHandler;
-import org.springframework.context.annotation.Bean;
-import org.springframework.integration.annotation.MessagingGateway;
-import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.messaging.MessageHandler;
 
 
 @SpringBootApplication
@@ -17,16 +11,4 @@ public class HyperlocalApplication {
   public static void main(String[] args) throws IOException {
 		SpringApplication.run(HyperlocalApplication.class, args);
   }
-  
-  @MessagingGateway(defaultRequestChannel = "pubsubOutputChannel")
-  public interface PubsubOutboundGateway {
-    void sendToPubsub(String text);
-  }
-
-  @Bean
-  @ServiceActivator(inputChannel = "pubsubOutputChannel")
-  public MessageHandler messageSender(PubSubTemplate pubsubTemplate) {
-  return new PubSubMessageHandler(pubsubTemplate, "testTopic");
-  }  
-
 }
