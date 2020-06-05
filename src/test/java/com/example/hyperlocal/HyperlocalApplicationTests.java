c0a3d44bccdb56d8853abd1126987ad58a66ef56
Diksha <dikshamodi@google.com>
2020-06-05 04:04:15 +0000

    Setup: Initial Setup and Build files
    Setup: Removed the autogenerated files
    
    Setup: Removed the autogenerated files
    
    Setup: Removed the autogenerated files

---
+++ src/test/java/com/example/hyperlocal/HyperlocalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hyperlocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperlocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
