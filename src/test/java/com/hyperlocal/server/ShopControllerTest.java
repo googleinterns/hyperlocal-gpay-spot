568b16fbebc49078256de9713fc1161a265ccc82
Diksha <dikshamodi@google.com>
2020-06-10 04:29:54 +0000

    Tests: Insert Shop

--- src/test/java/com/hyperlocal/server/ShopControllerTest.java
+++ src/test/java/com/hyperlocal/server/ShopControllerTest.java
@@ -1,11 +1,8 @@
 package com.hyperlocal.server;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
-import java.util.concurrent.CompletableFuture;
+import com.google.gson.JsonParser;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -25,9 +22,8 @@ public class ShopControllerTest {
   @Test
   public void shouldInsertShop() throws Exception {
     String TEST_URL = "{\"ShopID\":3,\"MerchantID\": \"4\",\"ShopName\":\"Test Shop\", \"Latitude\": 23.2323,\"Longitude\":23.53656, \"AddressLine1\":\"S-12\", \"TypeOfService\":\"Test\"}";
-    CompletableFuture<String> future = CompletableFuture.completedFuture(TEST_URL);
     controller.insertShop(TEST_URL);
-    verify(controller).insertShop(TEST_URL);
+    verify(controller).insertNewShop(JsonParser.parseString(TEST_URL).getAsJsonObject());
   }
 
 }
