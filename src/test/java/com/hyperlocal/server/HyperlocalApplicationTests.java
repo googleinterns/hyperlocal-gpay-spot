5e389dae3e58161765b88470a58fba8229e71ca8
Diksha <dikshamodi@google.com>
2020-06-10 11:40:53 +0000

    Fix: Indentation

--- src/test/java/com/hyperlocal/server/HyperlocalApplicationTests.java
+++ src/test/java/com/hyperlocal/server/HyperlocalApplicationTests.java
@@ -5,20 +5,20 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class HyperlocalApplicationTests {
-  
+
   @Autowired
   private MerchantController merchantController;
-  
+
   @Autowired
   private ShopController shopController;
 
-	@Test
-	void contextLoads() {
+  @Test
+  void contextLoads() {
     System.out.println("Context loads running");
     assertThat(merchantController).isNotNull();
     assertThat(shopController).isNotNull();
-	}
-
+  }
 }
