bc6b05eef5e2f341d93efbac3d3444973dfdee48
Diksha <dikshamodi@google.com>
2020-06-11 06:41:39 +0000

    Refactor: Remove unwanted indent

--- src/test/java/com/hyperlocal/server/HyperlocalApplicationTests.java
+++ src/test/java/com/hyperlocal/server/HyperlocalApplicationTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
 @SpringBootTest
 class HyperlocalApplicationTests {
 
