45597017defe7361f8a6af07c72ecc8ce5c317fe
onish <onish@google.com>
2020-06-22 14:49:59 +0530

    chore: packaged ReactJS app with Spring backend

---
+++ frontend/src/routes.js
@@ -0,0 +1,13 @@
+const ROUTES = {
+    'merchant': {
+        'dashboard' : '/my/dashboard',
+        'catalog': '/my/catalog',
+        'shopInfo': '/my/shop-info',
+        'onboarding': {
+            'shopInfo': '/onboarding/shop-info',
+            'catalog': '/onboarding/catalog'
+        }
+    }
+};
+
+export default ROUTES;
