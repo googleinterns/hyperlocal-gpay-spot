64318e41057dee87dcab1f5c65ea9f9482335d99
onish <onish@google.com>
2020-06-29 15:29:09 +0530

    fix: page load off on onboarding.catalog screen

--- frontend/src/Pages/Onboarding/Catalog.js
+++ frontend/src/Pages/Onboarding/Catalog.js
@@ -10,7 +10,7 @@ class Catalog extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        pageLoading: true,
+        pageLoading: false,
         showAddForm: false,
         addForm: {
           'serviceName': '',
